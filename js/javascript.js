098c17adcd74a2d2181ff3b51543d906b44d813e
PaulinPerera <paulinperera007@gmail.com>
2025-06-22 23:10:18 -0300

    mudei algumas bostas

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,40 @@
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert" data-bs-theme="dark">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert('EXTREMAMENTE PERIGOSO, SE VOCÊ OUVIR SUA CANÇÃO, ESTARA MORTO EM POUCOS INSTANTES, ESCONDASE E TAPE OS OUVIDOS', 'danger')
+  })
+}
+
+let lastScrollTop = 0;
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+        // rolando para baixo
+        navbar.style.top = '-80px'; // Esconde
+    } else {
+        // rolando para cima
+        navbar.style.top = '0'; // Mostra
+    }
+
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // evita valor negativo
+});
+
 //
 // SmoothScroll for websites v1.4.10 (Balazs Galambosi)
 // http://www.smoothscroll.net/
@@ -788,3 +825,62 @@ else
     window.SmoothScroll = SmoothScroll;
 
 })();
+
+function mostrarTexto1() {
+    document.getElementById('texto-dinamico').innerHTML = `<h2>Descrições</h2>
+        <q>Embora enganosamente bela em aparência e melodia, a Canção da Morte tem uma qualidade letal que
+        lembra a Viúva Negra. Habitando uma ilha exuberante além do arquipélago, este dragão vocaliza seu
+        belo e hipnótico chamado de sereia para atrair outros dragões, que ele então almeja como presas. Em
+        vez de usar fogo para atacar, a Canção da Morte dispara uma substância líquida que aprisiona e
+        preserva as vítimas enquanto endurece em um casulo âmbar rígido que é penetrável apenas por chamas.
+        Normalmente, a Canção da Morte escolhe capturar apenas dragões, mas também ataca humanos azarados o
+        suficiente para chegar à sua ilha. Suas principais fraquezas são os Tambores do Trovão, que se
+        encontram principalmente imunes devido à canção de ninar letal da Canção da Morte devido à audição
+        deficiente, e pequenos espaços fechados que podem torná-la vulnerável à sua própria canção.</q>
+        <br><br>
+        <q>Este Dragão usa seu chamado melódico para atrair seus alvos para as densas selvas da Ilha da
+        Melodia. Lá, ele pode observá-los enquanto eles buscam a fonte da música.</q>
+        <br><br>
+        <q>Belo como o nascer do sol e tão brutal quanto um Berserker, o Canto da Morte é uma espécie de dragão
+        notoriamente hipnótica. Ele não cospe fogo, mas cospe uma substância pegajosa semelhante ao âmbar
+        para aprisionar suas vítimas. Incentivamos qualquer aluno que confronte este dragão da Classe
+        Mistério a ter cuidado, pois o Canto da Morte é tão selvagem quanto um tufão!
+        Com suas escamas vibrantes, babados e chamado encantador, é fácil se encantar com os Cantos da
+        Morte, mas cuidado! Sob esse exterior resplandecente, esconde-se uma espécie invasora e canibal.
+        Eles usam seu canto para atrair dragões, apenas para prendê-los em um casulo de âmbar. Existem
+        apenas algumas criaturas com a capacidade de enfrentar um Canto da Morte ilesos. Os Tambores do
+        Trovão são imunes ao chamado do Canto da Morte porque têm audição fraca!</q>
+      `;
+}
+
+function mostrarTexto2() {
+    document.getElementById('texto-dinamico').innerHTML = `<h2>Características</h2><p>Asas coloridas</p>
+    <p>Barbatanas na cabeça</p>
+    <p>Longos espinhos ao longo das costas</p>
+    <p>Chifres longos</p>
+    <p>Barbelas</p>`;
+}
+
+function mostrarTexto3() {
+    document.getElementById('texto-dinamico').innerHTML = `<h2>Habilidades</h2><h3>Tipo de tiro:</h3><ul><p>Substância semelhante ao âmbar que endurece rapidamente</p></ul><h3>Outros:</h3><ul><p>Canção de atração semelhante a uma sereia</p><p>Imunidade a enguias</p><p>Imunidade às suas próprias canções de atração</p></ul>`;
+}
+
+function mostrarTexto4() {
+    document.getElementById('texto-dinamico').innerHTML = ``;
+}
+
+function mostrarTexto5() {
+    document.getElementById('texto-dinamico').innerHTML = `<h2>Descrições</h2><p>O Quaken Catastrófico pode se chocar contra o chão e criar enormes ondas de choque que podem derrubar dragões no ar.
+Como um dragão da Classe Boulder , o Quaken Catastrófico é literalmente o que ele come — já que ele gosta do consumo de pedras. Essa preferência alimentar é a causa de sua tendência a residir em terrenos rochosos, onde ele pode vagar a pé e obter sustento enquanto viaja. Forte e corpulento, esse dragão depende de seu tamanho como o mecanismo de defesa final. Quando ameaçado, sua reação imediata é entrar no modo de proteção e rolar em uma bola como um tatu gigante. Uma vez preso nessa posição, ele rapidamente assume a ofensiva, se chocando contra o chão para criar enormes ondas de choque que derrubarão dragões, estejam eles em terra ou no ar. A falta de interação humana tornou esses dragões cautelosos ao longo do tempo, mas desde que Soluço e os Cavaleiros de Dragão os apresentaram aos humanos, eles estão lentamente se tornando menos pedregosos e mais atenciosos.</p>`;
+}
+
+function mostrarTexto6() {
+    document.getElementById('texto-dinamico').innerHTML = `<h2>Características</h2><p>Cheio de espinhos, Mandíbula
+triplamente dividida, Pele
+dura como rocha</p>`;
+}
+
+function mostrarTexto7() {
+    document.getElementById('texto-dinamico').innerHTML = `<h2>Habilidades</h2><p>Enrolando-se em uma pedra,
+criando uma onda de choque</p>`;
+}
